e5a3509bbd474315faf45e1528efafdab83ca4e6
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-01-09 13:22:06 +0300

    replaces an element of a list at a specific position (like in C)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if element < 0 or element >= len(my_list):
+    if element < 0 and element >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
