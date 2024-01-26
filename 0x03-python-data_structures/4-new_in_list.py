d86e541cfff33648782856ceddb51f3ae2cb6246
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-01-26 16:03:57 +0300

    replaces an item in a list without changing the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return my_copy
+        return list_copy
 
     list_copy[idx] = element
     return list_copy
