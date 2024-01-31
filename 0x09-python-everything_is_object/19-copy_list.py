8df34d4c11208d5241e8332061b90b1816483e1e
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-01-31 17:36:21 +0300

    copies a list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""defines a function copy_list"""
-
-
 def copy_list(l):
-    """returns a copy of a list
-    Args:
-        l: list to copy
-    """
     if l:
         l = [:]
