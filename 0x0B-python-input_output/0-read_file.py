3d79a1268b1ef3cf8d4d2ba1a08bd0fba4060c8b
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-02-06 17:31:27 +0300

    reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to sdtout"""
-    with open("filename", encoding="utd-8") as my_file:
+    with open(filename, encoding="utd-8") as my_file:
         print(my_file.read(), end="")
