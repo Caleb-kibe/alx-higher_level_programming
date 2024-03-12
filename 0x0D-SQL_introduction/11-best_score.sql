ac76afc57ea70ee35e542a29a8ec8e58b5d84fe9
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-12 23:52:01 +0300

    lists the names and scores >= 10 from the second_table starting from the highest

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records with a score >= 10 in the second table
 -- Displays both the score and the name in that order
 -- Displays scores in order starting from the top score
-SELECT `score`, `name` FROM second_table WHERE `score` >= 10 AND ORDER BY DESC;
+SELECT `score`, `name` FROM second_table WHERE `score` >= 10 ORDER BY `score` DESC;
