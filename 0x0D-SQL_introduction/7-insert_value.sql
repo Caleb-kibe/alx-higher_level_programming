8f6703ac25f5964140de8c6fb4f21d8ff2c1c7ff
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-12 22:56:38 +0300

    inserts a new row into the table `first_table`

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Inserts a new row in the table `first_table` in the database hbtn_0c_0
-INSERT INTO `first_table` (`id` INT, `name` VARCHAR(256)) VALUES (89, "Best Scholl");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best Scholl");
