8a13c348bb176d57c3d3ae0ed43d4f44c570f839
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-12 23:39:04 +0300

    lists all records of score and name from the second_table starting from the top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all of the table `second_table` of the database hbtn_0c_0
+-- Displays both the score and the name in that order
+-- Displays record ordered by score (top first)
+SELECT `score`, `name` FROM second_table ORDER BY `score` DESC;
