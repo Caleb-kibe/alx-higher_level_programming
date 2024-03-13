282ca7e2b0ccdb3e782f29ef1fc9da11287f129f
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-13 17:40:17 +0300

    creates a database hbtn_0d_usa and a table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Creates a database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Creates a table states with columns id and name
+CREATE TABLE IF NOT EXISTS states (`id` INT UNIQUE AUTO_INCREMENT, `name` VARCHAR(256), PRIMARY KEY(id));
