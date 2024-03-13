fbe2d576574700e3619017cd5f9e28e1607f8a35
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-13 16:44:46 +0300

    lists all priveleges of MYSQL users user_0d_1 and user_0d_2 on my server

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of MYSQL users user_0d_1 ansd user_0d_2 on the server
-SHOW GRANTS FOR `user_0d_1` @ `localhost`;
-SHOW GRANTS FOR `user_0d_2` @ `localhost`;
+SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
