b71c03d4eb1e1851dd8e24ccd58daf994bb56de3
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-13 17:28:19 +0300

    creates a table id_not_null with id value set to 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- Creates a table id_not_null with description id and name
 -- The default value of id is set to 1
-CREATE TABLE IF NOT EXISTS id_not_null (`id=1` INT, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (`id` INT DEFAULT 1, `name` VARCHAR(256));
