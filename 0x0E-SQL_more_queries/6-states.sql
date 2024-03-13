04c08bf23d88dc4d53606f63d62e677a3be3d2fc
caleb kibe <kiplagatcaleb2@gmail.com>
2024-03-13 17:42:15 +0300

    creates a database hbtn_0d_usa and a table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,6 @@
 -- Creates a database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the database
+USE hbtn_0d_usa;
 -- Creates a table states with columns id and name
 CREATE TABLE IF NOT EXISTS states (`id` INT UNIQUE AUTO_INCREMENT, `name` VARCHAR(256), PRIMARY KEY(id));
