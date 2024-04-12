fb431eae3b98656d217133e7a20f82ef8c26f38b
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-12 16:21:44 +0300

    Creates a table states using python class states

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This file contains a class definition of State and an instance
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that inherits from Base
+    Attribute:
+            id: Id state
+            name: Name of the state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
