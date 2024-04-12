652291e0d6c8ee1840060e8dd2adfdf4756bd0d4
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-12 18:56:52 +0300

    lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name\
+    cursor.execute("SELECT * FROM states WHERE UPPER(name)\
                     LIKE 'N%' ORDER BY states.id ASC")
 
     states = cursor.fetchall()
