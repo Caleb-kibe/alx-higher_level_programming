9e0cc6b8a6622ec861ca50bf0420a74feda26219
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-25 22:56:26 +0300

    Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script takes in a URL,
+# sends a POST request to the passed URL,
+# and displays the body of the response
+
+email="test@gmail.com"
+subject="I will always be here for PLD"
+
+response=$(curl -s -X POST -d "email=$email&subject=$subject" "$1")
+echo "$response"
