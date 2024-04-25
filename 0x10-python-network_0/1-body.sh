6ac272bca2dbe8d0a7cff33f18568602526dc83d
caleb-kibe <kiplagatcaleb2@gmail.com>
2024-04-25 13:40:09 -0700

    Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -3,15 +3,4 @@
 # and displays the body of the response
 # Only displays the body of a 200 status code response
 
-url="$1"
-
-response=$(curl -s "$url")
-
-status_code=$(echo "$response" | head -n 1 | cut -d ' ' -f2)
-
-if [[ "$status_code" -eq 200 ]]; then
-	body=$(echo "$response" | sed '/^$/d')
-	echo "Route $body"
-else
-  echo "Error: Status code is not 200 (Found: $status_code)"
-fi
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
