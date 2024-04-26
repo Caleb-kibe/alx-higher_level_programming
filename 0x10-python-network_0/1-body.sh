19b325c5d11609ced19a72673d6d890a1db86cc0
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-26 17:40:32 +0300

    added an empty line at the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,5 +2,5 @@
 # This script takes in the URL, sends a GET request to the URL
 # and displays the body of the response
 # Only displays the body of a 200 status code response
-
 if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
+
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,9 +2,8 @@
 # This script takes in a URL,
 # sends a POST request to the passed URL,
 # and displays the body of the response
-
 email="test@gmail.com"
 subject="I will always be here for PLD"
-
 response=$(curl -s -X POST -d "email=$email&subject=$subject" "$1")
 echo "$response"
+
